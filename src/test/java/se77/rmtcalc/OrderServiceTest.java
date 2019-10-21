9cc5788925ec3bd9e9d985698378bbcbf68615d0
Ernesto Elias-Nieland <superernie77@gmail.com>
2019-10-21 12:04:45 +0200

    Update OrderServiceTest.java

--- src/test/java/se77/rmtcalc/OrderServiceTest.java
+++ src/test/java/se77/rmtcalc/OrderServiceTest.java
@@ -16,14 +16,18 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import se77.AppStarter;
+
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = AppStarter.class)
 public class OrderServiceTest {
     
     @Autowired
